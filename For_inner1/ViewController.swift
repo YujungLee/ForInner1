0ff11c988d63e630f735cf17c1cee8e07fbd8dd1
Yujung Lee <yujunglee@yujungui-macbook-pro.local>
2016-06-21 22:56:24 +0900

    Initial Commit

---
+++ For_inner1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  For_inner1
+//
+//  Created by Yujung Lee on 2016. 6. 21..
+//  Copyright © 2016년 YujungLee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
